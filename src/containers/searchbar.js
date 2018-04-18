22a54cd7b33ed7193814bfa56e491a8a9ebc81d4
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-18 15:49:13 -0600

    adds event handler to searchbar

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -4,10 +4,25 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input type="text"/>
+                <input 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    type="text"/>
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
