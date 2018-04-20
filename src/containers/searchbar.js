0f6fb5d22d14b4983382dedd60b8799360d16ed4
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-20 11:16:14 -0600

    spices up the form a bit

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -9,18 +9,27 @@ export default class SearchBar extends Component {
 
         this.state = { term: '' };
 
-        //
+        // binds context of searchBar component state to onInputChange function
+        // overwriting the local method with a new one bound to the context of SearchBar container
         this.onInputChange = this.onInputChange.bind(this);
-    }
+    };
 
     onInputChange(event) {
         console.log(event.target.value);
         this.setState({ term: event.target.value });
-    }
+    };
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // we need an api call to fetch weather data
+    };
 
     render() {
         return (
-            <form className="input-group">
+            <form 
+                onSubmit={this.onFormSubmit}
+                className="input-group">
                 <input 
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
@@ -33,6 +42,4 @@ export default class SearchBar extends Component {
             </form>
         );
     };
-
-
 };
