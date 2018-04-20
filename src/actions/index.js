c923cb3e59c9dab0e0d670e7bc7d457e470489a5
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-20 12:07:29 -0600

    starts to set up api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
-const API_KEY = 'fe411f5c5d3f1095f5f6c3b717d9c5c5';
+// env import
+import API_KEY from '../config/env';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// create a single variable that holds an action type
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// action creator that is going to make an api call to weathermap.org
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    }
+}
--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-
+// environment import
 
 export default class SearchBar extends Component {
 
